135277b6490d40c2d4dc1fa7dc18b9240fb80d1d
Reedcooley <106986310+Reedcooley@users.noreply.github.com>
2026-01-12 21:22:44 +0100

    commit

--- app.py
+++ app.py
@@ -2,23 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Writing page
-@app.route("/writing")
+@app.route("/writing/")
 def writing():
     return render_template("writing.html")
 
-# Design page
-@app.route("/design")
+@app.route("/design/")
 def design():
     return render_template("design.html")
 
-# Process page
-@app.route("/process_page")
+@app.route("/process_page/")
 def process_page():
     return render_template("process.html")
 
--- freeze.py
+++ freeze.py
@@ -2,8 +2,16 @@ from flask_frozen import Freezer
 from app import app
 
 app.config["FREEZER_RELATIVE_URLS"] = True
+app.config["FREEZER_DESTINATION"] = "frozen"
 
 freezer = Freezer(app)
+
+@freezer.register_generator
+def url_generator():
+    yield "/"
+    yield "/writing/"
+    yield "/design/"
+    yield "/process_page/"
 
 if __name__ == "__main__":
     freezer.freeze()
